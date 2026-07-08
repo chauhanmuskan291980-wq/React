be86f79ec26fbdb599ccb27d87a64ff9d0e7411b
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-07-08 21:52:04 +0530

    feat: add layout component and update routing for Home and About pages

--- reactRouter/src/assets/components/AboutUs.jsx
+++ reactRouter/src/assets/components/AboutUs.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+
 export default function About() {
     return (
         <div className="py-16 bg-white">
--- reactRouter/src/assets/components/footer.jsx
+++ reactRouter/src/assets/components/footer.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+
 export default function Footer() {
     return (
         <footer className="bg-white border-y">
